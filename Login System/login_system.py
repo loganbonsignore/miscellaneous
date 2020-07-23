3e935a8ce5cd6b7758d2de6aa2c9ebd8f2e9246a
Logan Bonsignore <Loganbon14@gmail.com>
2020-07-23 11:09:25 -0600

    Login edits

--- Login System/login_system.py
+++ Login System/login_system.py
@@ -1,6 +1,29 @@
 import os
 import pandas as pd
 
+def create_new_account(customer_df, private_df):
+    # collect user information
+    print("No Problem! Answer these questions to complete your account set up:")
+    first_name = input("What is your first name? :")
+    last_name = input("What is your last name? :")
+    city = input("What city do you live in? :")
+    phone = input("What is your phone number? :")
+    email = input("What is your email address? :")
+    username = input("What would you like your new username to be? :").lower()
+    password = input("What would you like your new password to be? :")
+    information = [first_name,last_name,city,phone,email]
+    private = [username,password]
+    # add new information to dataframe
+    new_row = {"First Name":first_name,"Last Name":last_name,"City":city,"Phone":phone,"Email":email}
+    customer_df = customer_df.append(new_row, ignore_index=True)
+    # add new login info to private dataframe
+    new_row_1 = {"Username":username,"Password":password}
+    private_df = private_df.append(new_row_1, ignore_index=True)
+    # save both dataframes to csv
+    customer_df = customer_df.to_csv(customer_list_path, index=False)
+    private_df = private_df.to_csv(private_path, index=False)
+    print("Your account has been created!! Enjoy the benefits of our website")
+
 # get resource file paths
 customer_list_path = os.path.join("Resources/customer_list.csv")
 private_path = os.path.join("Resources/private.csv")
@@ -29,58 +52,17 @@ if user_input.lower() == "y":
             break
         # if password is incorrect ask user to re-enter password
         elif (username == customer["Username"]).bool() & (password != customer["Password"]).bool():
-            password = input("That password is incorrect. Please try again.")       
+            password = input("That password is incorrect. Please try again.")
     # if username is not recognized, promt user to set up new account
     else:
         user_input = input("We don't have an account associated with that username. Would you like to set up a new account? (y/n)")
         # if user doesnt want to create new account, quit the program
         if user_input.lower() == "n":
-            print("Thanks for visiting our website. We hope to hear from you again soon.") 
+            print("Thanks for visiting our website. We hope to hear from you again soon.")
             exit()
         # if user wants to create account, prompt them to set up a new account and collect information
         elif user_input.lower() =="y":
-            # collect user information
-            print("No Problem! Answer these questions to complete your account set up:")
-            first_name = input("What is your first name? :")
-            last_name = input("What is your last name? :")
-            city = input("What city do you live in? :")
-            phone = input("What is your phone number? :")
-            email = input("What is your email address? :")
-            username = input("What would you like your new username to be? :").lower()
-            password = input("What would you like your new password to be? :")
-            information = [first_name,last_name,city,phone,email]
-            private = [username,password]
-            # add new information to dataframe
-            new_row = {"First Name":first_name,"Last Name":last_name,"City":city,"Phone":phone,"Email":email}
-            customer_df = customer_df.append(new_row, ignore_index=True)
-            # add new login info to private dataframe
-            new_row_1 = {"Username":username,"Password":password}
-            private_df = private_df.append(new_row_1, ignore_index=True)
-            # save both dataframes to csv
-            customer_df = customer_df.to_csv(customer_list_path, index=False)
-            private_df = private_df.to_csv(private_path, index=False)
-            print("Your account has been created!! Enjoy the benefits of our website")
-
+            create_new_account(customer_df, private_df)
 # if user doesn't have an account, prompt them to set up a new account and collect information
 elif user_input == "n":
-    # collect user information
-    print("No Problem! Answer these questions to complete your account set up:")
-    first_name = input("What is your first name? :")
-    last_name = input("What is your last name? :")
-    city = input("What city do you live in? :")
-    phone = input("What is your phone number? :")
-    email = input("What is your email address? :")
-    username = input("What would you like your new username to be? :").lower()
-    password = input("What would you like your new password to be? :")
-    information = [first_name,last_name,city,phone,email]
-    private = [username,password]
-    # add new information to dataframe
-    new_row = {"First Name":first_name,"Last Name":last_name,"City":city,"Phone":phone,"Email":email}
-    customer_df = customer_df.append(new_row, ignore_index=True)
-    # add new login info to private dataframe
-    new_row_1 = {"Username":username,"Password":password}
-    private_df = private_df.append(new_row_1, ignore_index=True)
-    # save both dataframes to csv
-    customer_df = customer_df.to_csv(customer_list_path, index=False)
-    private_df = private_df.to_csv(private_path, index=False)
-    print("Your account has been created!! Enjoy the benefits of our website")
+    create_new_account(customer_df, private_df)
